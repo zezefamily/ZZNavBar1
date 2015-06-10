9f5f2691a406502432772bb026bc9747bc51b496
zeze <18813155491@163.com>
2015-06-10 15:40:55 +0800

    Initial Commit

---
+++ ZZNavBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZNavBar
+//
+//  Created by zezefamily on 15/6/10.
+//  Copyright (c) 2015年 zezefamily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
